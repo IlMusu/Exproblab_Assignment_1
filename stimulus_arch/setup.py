64235f2e89318d34cdbd16f8056e99e9c754e090
IlMusu <mattia.musumeci@yahoo.it>
2022-11-28 12:40:15 +0100

    gifs and images

--- stimulus_arch/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-modules = generate_distutils_setup(
-    packages=['stimulus_arch'],
-    package_dir={'': 'scripts'}
-)
-setup(**modules)
